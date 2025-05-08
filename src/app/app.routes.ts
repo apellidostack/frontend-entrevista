022e64b04a8fb9fa885de6ecffdbb12411a3459f
apellidostack <apellidojoan4@gmail.com>
2025-05-08 12:56:19 -0400

    segunto commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { OrderPageComponent } from './pages/order-page/order-page.component';
 
 export const routes: Routes = [
     {path: 'inicio',title:'inicio', component: OrderPageComponent},
-    
+    {path: '', redirectTo:'/inicio',pathMatch:'full'},
 ];
